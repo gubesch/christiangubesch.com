38c9720a858055b838904b6ffe28f0bee4c3ecf3
cgubesch <christian.gubesch@bearingpoint.com>
2021-01-06 11:59:54 +0100

    add google analytics

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -78,7 +78,7 @@ module.exports = {
   siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
   pathPrefix: '/', // Note: it must *not* have a trailing slash.
   siteCover: '/images/cover.jpeg',
-  googleAnalyticsId: 'G-HFM3H8RPTB',
+  googleAnalyticsId: 'UA-257678720',
   background_color: '#ffffff',
   theme_color: '#25303B',
   fontColor: "#000000cc",
